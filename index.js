113d89f5c3e612dee47df59b49360a1f7b063600
Karina Muñoz Gonzalez <karinamzalez@gmail.com>
2017-09-04 23:26:51 -0600

    add mathjs

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
+var math = require('mathjs');
+
 class spiralMatrix {
   constructor(integer) {
     this.integer = integer;
   }
+
+  isPerfectSquare() {
+    // check
+    return n > 0 && Math.sqrt(n) % 1 === 0;
+  }
+
 }
 
 module.exports = spiralMatrix;
--- test/index-test.js
+++ test/index-test.js
@@ -3,9 +3,13 @@ const assert = chai.assert;
 const SpiralMatrix = require('../index.js');
 
 describe('SpiralMatrix', function() {
-  it('takes in an integer', function() {
-    var spiralMatrix = new SpiralMatrix(24);
+  var spiralMatrix = new SpiralMatrix(24);
 
+  it('takes in an integer', function() {
     assert.equal(spiralMatrix.integer, 24);
+  });
+
+  it('checks if given integer + 1 is perfect square', function() {
+    
   });
 });
