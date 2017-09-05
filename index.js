4ab628933f888690135e52e5f97971aab61a8a33
Karina Muñoz Gonzalez <karinamzalez@gmail.com>
2017-09-05 00:42:45 -0600

    generates array matrix with zeros as placeholders

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-var math = require('mathjs');
+const math = require('mathjs');
 
 class spiralMatrix {
   constructor(integer) {
@@ -6,10 +6,17 @@ class spiralMatrix {
   }
 
   isPerfectSquare() {
-    // check
-    return n > 0 && Math.sqrt(n) % 1 === 0;
+    //add one to integer to account for center at 0
+    var integer = this.integer + 1;
+    // check integer is positive and has no remainder after finding square root
+    return math.sqrt(integer) % 1 === 0;
   }
 
+  generateMatrix() {
+    //generate array matrix with zeros as placeholders
+    var dimension = math.sqrt(this.integer + 1);
+    return math.zeros([dimension, dimension]);
+  }
 }
 
 module.exports = spiralMatrix;
--- test/index-test.js
+++ test/index-test.js
@@ -1,8 +1,9 @@
 const chai = require('chai');
 const assert = chai.assert;
 const SpiralMatrix = require('../index.js');
+const math = require('mathjs');
 
-describe('SpiralMatrix', function() {
+describe('SpiralMatrix with perfect square', function() {
   var spiralMatrix = new SpiralMatrix(24);
 
   it('takes in an integer', function() {
@@ -10,6 +11,14 @@ describe('SpiralMatrix', function() {
   });
 
   it('checks if given integer + 1 is perfect square', function() {
-    
+    assert.equal(spiralMatrix.isPerfectSquare(), true);
+  });
+
+  it('generates Matrix with perfect square dimensions', function() {
+    var matrix = spiralMatrix.generateMatrix();
+
+    //math.size() returns array with both dimenstions
+    assert.equal(math.size(matrix)[0], 5);
+    assert.equal(math.size(matrix)[1], 5);
   });
 });
